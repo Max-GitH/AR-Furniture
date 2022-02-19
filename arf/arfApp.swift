db20d9fa47481eeaa8ec47641191c2696de1e1a1
Max <mnonisa3@getech.edu>
2022-02-18 20:53:18 -0800

    Initial Commit

---
+++ arf/arfApp.swift
@@ -0,0 +1,17 @@
+//
+//  arfApp.swift
+//  arf
+//
+//  Created by Max Nonisa on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct arfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
